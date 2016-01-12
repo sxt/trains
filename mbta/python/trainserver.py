a6da6f1b2dc2934eb315072413b754880b500d49
Stephen Turner <sturner@mit.edu>
2016-01-12 12:58:57 -0500

    Debugging file locations on heroku

--- mbta/python/trainserver.py
+++ mbta/python/trainserver.py
@@ -5,15 +5,16 @@ from twisted.web.static import File
 from twisted.web.resource import Resource
 from sslproxy import SSLReverseProxyResource
 import os
+import sys
 
 wwwroot = os.environ.get('wwwroot')
 mbta_host = os.environ.get('mbta_host')
 mq_host = os.environ.get('mq_host')
 mq_path = os.environ.get('mq_path')
 
-print mq_path
-
 fileResource = File(os.path.abspath(wwwroot))
+print "*** wwwroot ***: " + os.path.abspath(wwwroot)
+sys.stdout.flush()
 mbtaProxyResource = proxy.ReverseProxyResource(mbta_host, 80, '')
 ironmqProxyResource = SSLReverseProxyResource(mq_host, 443, mq_path)
 rootResource = Resource()
