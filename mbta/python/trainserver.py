577ef9fd7f7c6461c340cde2f24e13effaad87c7
sxt <sxturner@hotmail.com>
2018-03-17 18:12:26 -0400

    Removing references to mq (iron.io) - no longer have an account there

--- mbta/python/trainserver.py
+++ mbta/python/trainserver.py
@@ -9,19 +9,15 @@ import sys
 
 wwwroot = os.environ.get('wwwroot')
 mbta_host = os.environ.get('mbta_host')
-mq_host = os.environ.get('mq_host')
-mq_path = os.environ.get('mq_path')
 
 fileResource = File(os.path.abspath(wwwroot))
 print "*** wwwroot ***: " + os.path.abspath(wwwroot)
 print os.listdir('.')
 sys.stdout.flush()
 mbtaProxyResource = proxy.ReverseProxyResource(mbta_host, 80, '')
-ironmqProxyResource = SSLReverseProxyResource(mq_host, 443, mq_path)
 rootResource = Resource()
 
 rootResource.putChild("mbta", mbtaProxyResource)
-rootResource.putChild("mq", ironmqProxyResource)
 rootResource.putChild("local", fileResource)
 
 site = server.Site(rootResource)
